841e1f90560c45078a0b55c1f606901abf4ea7ff
patrickfrey <patrickpfrey@yahoo.com>
2016-12-09 11:20:53 +0100

    adapted interface changes in analyzer: integrated pattern matching

--- src/objects_gen.hpp
+++ src/objects_gen.hpp
@@ -352,6 +352,33 @@ public:
 	virtual void defineSubDocument(
 			const std::string& p1, 
 			const std::string& p2);
+	virtual void definePatternMatcherPostProc(
+			const std::string& p1, 
+			PatternMatcherInstanceInterface* p2, 
+			PatternTermFeederInstanceInterface* p3);
+	virtual void definePatternMatcherPreProc(
+			const std::string& p1, 
+			PatternMatcherInstanceInterface* p2, 
+			PatternLexerInstanceInterface* p3, 
+			const std::vector<std::string>& p4);
+	virtual void addSearchIndexFeatureFromPatternMatch(
+			const std::string& p1, 
+			const std::string& p2, 
+			const std::vector<NormalizerFunctionInstanceInterface*>& p3, 
+			const analyzer::FeatureOptions& p4);
+	virtual void addForwardIndexFeatureFromPatternMatch(
+			const std::string& p1, 
+			const std::string& p2, 
+			const std::vector<NormalizerFunctionInstanceInterface*>& p3, 
+			const analyzer::FeatureOptions& p4);
+	virtual void defineMetaDataFromPatternMatch(
+			const std::string& p1, 
+			const std::string& p2, 
+			const std::vector<NormalizerFunctionInstanceInterface*>& p3);
+	virtual void defineAttributeFromPatternMatch(
+			const std::string& p1, 
+			const std::string& p2, 
+			const std::vector<NormalizerFunctionInstanceInterface*>& p3);
 	virtual analyzer::Document analyze(
 			const std::string& p1, 
 			const analyzer::DocumentClass& p2) const;
@@ -698,15 +725,11 @@ public:
 			unsigned int p1, 
 			unsigned int p2, 
 			const std::string& p3);
+	virtual unsigned int getLexem(
+			const std::string& p1) const;
 	virtual unsigned int getSymbol(
 			unsigned int p1, 
 			const std::string& p2) const;
-	virtual std::vector<analyzer::PatternLexem> mapTerms(
-			const std::vector<analyzer::Term>& p1) const;
-	virtual std::vector<analyzer::Term> mapResults(
-			const std::string& p1, 
-			const std::vector<analyzer::PatternMatcherResult>& p2, 
-			const std::vector<analyzer::Term>& p3) const;
 };
 
 class PatternTermFeederImpl
@@ -814,6 +837,23 @@ public:
 			const std::string& p2, 
 			TokenizerFunctionInstanceInterface* p3, 
 			const std::vector<NormalizerFunctionInstanceInterface*>& p4);
+	virtual void definePatternMatcherPostProc(
+			const std::string& p1, 
+			PatternMatcherInstanceInterface* p2, 
+			PatternTermFeederInstanceInterface* p3);
+	virtual void definePatternMatcherPreProc(
+			const std::string& p1, 
+			PatternMatcherInstanceInterface* p2, 
+			PatternLexerInstanceInterface* p3, 
+			const std::vector<std::string>& p4);
+	virtual void addSearchIndexFeatureFromPatternMatch(
+			const std::string& p1, 
+			const std::string& p2, 
+			const std::vector<NormalizerFunctionInstanceInterface*>& p3);
+	virtual void defineMetaDataFromPatternMatch(
+			const std::string& p1, 
+			const std::string& p2, 
+			const std::vector<NormalizerFunctionInstanceInterface*>& p3);
 	virtual QueryAnalyzerContextInterface* createContext() const;
 };
 
